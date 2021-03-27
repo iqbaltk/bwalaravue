700818de20e951ae31806caccf110e8d420dbe5c
iqbaltk <iqbaltri156@gmail.com>
2021-03-27 17:37:29 +0700

    upload ke github

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <!-- Page Preloder -->
+    <!-- <div id="preloder">
+        <div class="loader"></div>
+    </div> -->
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+     
+  </div>
+</template>
+
+<script>
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenShayna from "@/components/WomenShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
